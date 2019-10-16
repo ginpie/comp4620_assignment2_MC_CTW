a01161d473919d3ae02d270e157d6783a2f49271
Jinpei Chen <jinpei.chen@anu.edu.au>
2019-10-16 19:02:02 +1100

    update

--- pyaixi/environments/kuhn_poker.py
+++ pyaixi/environments/kuhn_poker.py
@@ -16,7 +16,7 @@ sys.path.insert(0, PROJECT_ROOT)
 # define actions
 kuhn_action_enum = util.enum('aPass', 'aBet')
 
-# define observations 
+# define observations
 kuhn_observation_enum = util.enum('oPass', 'oBet', 'oK', 'oQ', 'oJ')
 
 # define rewards: there are fout rewards, and no draw
@@ -44,174 +44,120 @@ class KuhnPoker(environment.Environment):
         environment.Environment.__init__(self, options=options)
 
         self.valid_actions = list(kuhn_action_enum.keys())
-        self.valid_obeservations = list(kuhn_observation_enum.keys())
+        self.valid_observations = list(kuhn_observation_enum.keys())
         self.valid_rewards = list(kuhn_reward_enum.keys())
         self.reward = 0
-        
-        # dealing. the first card is for the agent, and the second card is for the opponent
-        self.dealer = random.sample(self.valid_observations[2:4], 2)
-        # the agent gets the first card
-        self.card = self.dealer[0]
-        # now the agent can't see the card hold by the opponent. assigned when showtown
-        self.oppo_card = self.dealer[1]
-        self.showdown = False
-        # the second action the opponent might take. if it adopts Nash strategy, the second action of the opponent is
-        # always pass, but it will be assigned only in one condition: when the first action of the opponent is
-        # pass and the action of the agent is bet
-        self.next_step = None
+        self.agent_action = 'aPass'
+        self.observation = ""
 
-        # initialize the observation and reward with blinds.
-        # [0:the agent's action,
-        #  1:the agent's card,
-        #  2:the opponent's action,
-        #  3:the opponent's card,
-        #  4:the next action of the opponent,
-        #  5:whether showdown]
-        self.observation = enumerate([self.action, self.card, None, None, self.next_step, self.showdown])
-        # initialize gamma in Nash strategy
-        self.gamma = 0.5
-        self.reset()
+        self.game_reset()
+
     def perform_action(self, action):
         """ Receives the agent's action and calculates the new environment percept.
         """
-
         assert self.is_valid_action(action)
-        # assert self.card != self.oppo_card
-        # assert self.shownown == True and self.oppo_card != None and self.oppo_action !=None
-        # assert self.shownown == False and self.oppo_card == None and self.oppo_action == None
-
-        # define a function to choose one action with given probability
-        def random_action(action_list, probability_list):
-            index = random.uniform(0,1)
-            assert len(action_list)==len(probability_list)
-            assert sum(probability_list) == 1
-            for i in range(len(action_list)):
-                if index >= probability_list[i] and index < probability_list[i+1]:
-                    return action_list[i]
-
-        # define the strategy of the opponent
-        def oppo_first_action(self, oppo_card):
-            if oppo_card == K:
-                return random_action([oBet, oPass], [self.gamma, 1-self.gamma])
-            elif oppo_card == Q:
-                return 'oPass'
-            else:
-                alpha = self.gamma/3
-                return random_action([oBet, oPass], [alpha, 1-alpha])
-
-        def oppo_second_action(self):
-            beta = (1+self.gamma)/3
-            return random_action([oBet, oPass], [beta, 1-beta])
 
         # Save the action
-        self.action = action
-        oppo_action = oppo_first_action(self, self.oppo_card)
-        self.observation[2] = oppo_action
+        self.agent_action = action
 
-        # if the opponent pass
-        if oppo_action == oPass:
-            # if the agent pass
-            if self.action == aPass:
-                # showndown and assign value to opponent action & card
-                self.showdown = True
-                self.observation[3] = self.oppo_card
-                # if hold K, win
-                if self.card == K:
-                    if self.oppo_card == Q:
-                        self.reward = rWin1
-                    if self.oppo_card == J:
-                        self.reward = rWin1
-                #  if hold Q
-                if self.card == Q:
-                    if self.oppo_card == K:
-                        self.reward = rLose1
-                    if self.oppo_card == J:
-                        self.reward = rWin1
-                #  if hold J
-                if self.card == Q:
-                    if self.oppo_card == K:
-                        self.reward = -rLose1
-                    if self.oppo_card == Q:
-                        self.reward = -rLose1
-            # if the agent bets
-            else:
-                second_action = oppo_second_action()
-                # if the opponent pass, win
-                if second_action == oPass:
-                    self.reward = rWin1
-                # if the opponent bet
-                else:
-                    self.showdown = True
-                    self.observation[3] = self.oppo_card
-                    # if hold K, win
-                    if self.card == K:
-                        if self.oppo_card == Q:
-                            self.reward = rWin2
-                        if self.oppo_card == J:
-                            self.reward = rWin2
-                    #  if hold Q
-                    if self.card == Q:
-                        if self.oppo_card == K:
-                            self.reward = rLose2
-                        if self.oppo_card == J:
-                            self.reward = rWin2
-                    #  if hold J
-                    if self.card == Q:
-                        if self.oppo_card == K:
-                            self.reward = rLose2
-                        if self.oppo_card == Q:
-                            self.reward = rLose2
-
-        # if the opponent bet
-        else:
-            # if the agent bet
-            self.showdown = True
-            self.observation[3] = self.oppo_card
-            if self.action == aBet:
-                # showndown and assign value to opponent action & card
-                self.showdown = True
-                self.observation[3] = self.oppo_card
-                # if hold K, win
-                if self.card == K:
-                    if self.oppo_card == Q:
-                        self.reward = rWin2
-                    if self.oppo_card == J:
-                        self.reward = rWin2
-                #  if hold Q
-                if self.card == Q:
-                    if self.oppo_card == K:
-                        self.reward = rLose2
-                    if self.oppo_card == J:
-                        self.reward = rWin2
-                #  if hold J
-                if self.card == Q:
-                    if self.oppo_card == K:
-                        self.reward = rLose2
-                    if self.oppo_card == Q:
-                        self.reward = rLose2
-            # if the agent passes, lose
-            else:
+        # opponent initially bets
+        if self.opponent_action == aBet:
+            # agent folds
+            if self.agent_action == aPass:
                 self.reward = rLose1
+            # agent calls
+            elif self.agent_action == aBet:
+                self.reward = (rLose2 if self.smallerThan(self.agent_card, self.opponent_card) else rWin2)
 
-        return (self.observation, self.reward)
+        # opponent initially passes
+        if self.opponent_action == aPass:
+            # agent checks
+            if self.agent_action == aPass:
+                self.reward = (rLose1 if self.smallerThan(self.agent_card, self.opponent_card) else rWin1)
+            # agent bets
+            elif self.agent_action == aBet:
+                # opponent calls or folds depending on his card
+                # K must call
+                if self.opponent_card == K:
+                    self.opponent_action = aBet
+                    self.reward = rLose2
+                # J must fold
+                elif self.opponent_card == J:
+                    self.opponent_action = aPass
+                    self.reward = rWin1
+                # Q has a probability
+                else:
+                    if random.random() < self.pb:
+                        self.opponent_action = aBet
+                        self.reward = (rLose2 if self.smallerThan(self.agent_card, self.opponent_card) else rWin2)
+                    else:
+                        self.opponent_action = aPass
+                        self.reward = rLose1
 
+        self.game_reset()
+        return self.observation, self.reward
+    # end def
+
+    def smallerThan(self, card1, card2):
+        if card1 == 'oJ':
+            return True
+        elif card1 == 'oK':
+            return False
+
+        if card2 == 'oJ':
+            return False
+        elif card2 == 'oK':
+            return True
     # end def
 
     def print(self):
         """ Returns a string indicating the status of the environment.
         """
-
-        message = "prediction: " + \
-                  ("pass" if self.action == aPass else "bet") + \
-                  ", observation: " + \
-                  ("the opponent passes" if self.observation[0] == oPass else "the opponent bets") + \
-                  ("I hold %s" % (self.observation[2])) + \
-                  ", reward: %d" % self.reward
+        message = "Prediction: " + \
+                  "Agent has %s" % self.agent_card + ", opponent has %s" % self.opponent_card + "\n" + \
+                  "Opponent decides to %s" % ("bet" if self.opponent_action == "aBet" else "pass") + \
+                  ", agent decides to %s" % ("bet" if self.agent_action == "aBet" else "pass") + "\n" + \
+                  "Result: " + \
+                  "agent " + ("wins" if self.reward == rWin1 or self.reward == rWin2 else "loses") + \
+                  ", reward: " + self.reward - 2
 
         return message
     # end def
 
-    def reset(self):
-        self.observation = enumerate([None, None, None, None, None, False])
-        self.reward = None
+    def game_reset(self):
+        # shuffle
+        self.dealer = random.sample([K, Q, J], 2)
+        # deal
+        self.agent_card = self.dealer[0]
+        self.opponent_card = self.dealer[1]
+
+        """
+        Choose an initial action for the opponent:
+        1. When having a Jack, opponent freely chooses the probability pj in [0,1/3]
+           with which he will bet  (otherwise he checks; if the other player bets, he should always fold).
+        2. When having a King, he should bet with the probability of 3 x pj  (otherwise he checks;
+           if the other player bets, he should always call).
+        3. When having a Queen he should always check, and if the other player bets after this check,
+           he should call with the probability of pj + 1/3.
+        """
+        pj = 0.3
+        pq = pj * 3
+        pk = pj + 1.0/3.0
+        self.pb = pj
+        if self.opponent_card == Q:
+            self.pb = pq
+        elif self.opponent_card == K:
+            self.pb = pk
+
+        # initial action of opponent
+        if self.opponent_card == Q:
+            self.opponent_action = aPass
+        elif random.random() > self.pb:
+            self.opponent_action = aPass
+        else:
+            self.opponent_action = aBet
+
+        # observation of the agent is a concatenation of opponent_action and agent_card
+        self.observation = self.opponent_action + self.agent_card
+
 # end class
